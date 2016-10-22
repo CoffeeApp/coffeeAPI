3df437e013fb55656028aaac4ed8fcce8cb17ae0
Apprentice <email": null>
2016-10-23 11:37:18 +1300

    Seeding shop_coffee table with all coffee options

--- data/seeds/shop_coffee.js
+++ data/seeds/shop_coffee.js
@@ -5,9 +5,22 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('shop_coffee').insert({coffee_id: 1, shop_id: 1, price: 3.5}),
-        knex('shop_coffee').insert({coffee_id: 2, shop_id: 1, price: 4.5}),
-        knex('shop_coffee').insert({coffee_id: 3, shop_id: 1, price: 4})
+        knex('shop_coffee').insert({coffee_id: 1, shop_id: 1, price: 1}),
+        knex('shop_coffee').insert({coffee_id: 2, shop_id: 1, price: 2}),
+        knex('shop_coffee').insert({coffee_id: 3, shop_id: 1, price: 3}),
+        knex('shop_coffee').insert({coffee_id: 4, shop_id: 1, price: 3.5}),
+        knex('shop_coffee').insert({coffee_id: 5, shop_id: 1, price: 4.5}),
+        knex('shop_coffee').insert({coffee_id: 6, shop_id: 1, price: 4}),
+        knex('shop_coffee').insert({coffee_id: 7, shop_id: 1, price: 5}),
+        knex('shop_coffee').insert({coffee_id: 8, shop_id: 1, price: 6.5}),
+        knex('shop_coffee').insert({coffee_id: 1, shop_id: 2, price: 10}),
+        knex('shop_coffee').insert({coffee_id: 2, shop_id: 2, price: 20}),
+        knex('shop_coffee').insert({coffee_id: 3, shop_id: 2, price: 30}),
+        knex('shop_coffee').insert({coffee_id: 4, shop_id: 2, price: 35}),
+        knex('shop_coffee').insert({coffee_id: 5, shop_id: 2, price: 45}),
+        knex('shop_coffee').insert({coffee_id: 6, shop_id: 2, price: 40}),
+        knex('shop_coffee').insert({coffee_id: 7, shop_id: 2, price: 50}),
+        knex('shop_coffee').insert({coffee_id: 8, shop_id: 2, price: 65})
       ]);
     });
 };
