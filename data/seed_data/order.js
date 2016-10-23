9d766a820d41a77b867b1369d9832160f74f1bd2
Apprentice <email": null>
2016-10-24 12:49:03 +1300

    Preparing seeding data 2

---
+++ data/seed_data/order.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    name: 'peter',
+    phone: '021 293 2329',
+    status: 'new',
+    shop_id: 1,
+    comment: 'on time',
+    new_date: new Date()
+  },
+  {
+    name: 'ben',
+    phone: '021 293 2899',
+    status: 'new',
+    shop_id: 1,
+    comment: 'no sugar, no milk',
+    new_date: new Date()
+  },
+  {
+    name: 'Thomas',
+    phone: '021 293 1231',
+    status: 'new',
+    shop_id: 2,
+    comment: 'a lot of sugar',
+    new_date: new Date()
+  },
+  {
+    name: 'Lisa',
+    phone: '021 123 5675',
+    status: 'new',
+    shop_id: 2,
+    comment: 'no coffee beans',
+    new_date: new Date()
+  }
+]
--- data/seeds/shop_coffee.js
+++ data/seeds/shop_coffee.js
@@ -1,14 +1,17 @@
-var seed_data = require('../seed_data')
-var shop_coffees = seed_data.shop_coffees
-var coffees = seed_data.coffees
-var order_details = seed_data.order_details
+var coffee = require('../seed_data/coffee')
+var order_detail = require('../seed_data/order_detail')
+var order = require('../seed_data/order')
+var shop_coffee = require('../seed_data/shop_coffee')
+var shop = require('../seed_data/shop')
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return Promise.all([
     knex('shop_coffee').del(),
     knex('order_detail').del(),
-    knex('coffee').del()
+    knex('order').del(),
+    knex('coffee').del(),
+    knex('shop').del()
   ]).then(() => {
     return knex('coffee')
       .insert(coffees)
