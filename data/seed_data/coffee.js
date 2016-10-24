5419c1875aa0c33519929df115f92b5eb6f0843f
Guanzhou Zhao <benzhao2866@gmail.com>
2016-10-24 17:51:29 +1300

    Client side patching works

--- data/seed_data/coffee.js
+++ data/seed_data/coffee.js
@@ -1,32 +1,32 @@
 module.exports = [
-        {
-            type: 'Short Black',
-            image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
-            description: 'A Short Black is what Australians call a shot of espresso. It is typically a 30ml espresso served in a small glass with a thick crema floating on top.'
-        },
-        {
-            type: 'Flat White',
-            image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
-            description: 'A type of coffee made with espresso and hot steamed milk, but without the froth characteristic of a cappuccino.'
-        },
-        {
-            type: 'Cold Press',
-            image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
-            description: 'Cold brew, or cold press, is coffee grounds steeped in water at cold or room temperature for an extended period.'
-        },
-        {
-            type: 'Macchiato',
-            image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
-            description: 'Macchiato, meaning "stained", is an espresso with a dash of foamed milk. At first sight it resembles a small cappuccino, but even if the ingredients are the same as those used for cappuccino, a macchiato has a much stronger and aromatic taste.'
-        },
-        {
-            type: 'Chai Latte',
-            image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
-            description: 'Numerous houses use the term chai latte to indicate that the steamed milk of a normal caffè latte is being flavoured with a spiced tea concentrate instead of with espresso.'
-        },
-        {
-            type: 'Mochaccino',
-            image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
-            description: 'Single shot poured over heaped teaspoon of hot chocolate powder and stirred. Top with velvety milk in a cappuccino style.'
-        },
-    ]
+  {
+    type: 'Short Black',
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
+    description: 'A Short Black is what Australians call a shot of espresso. It is typically a 30ml espresso served in a small glass with a thick crema floating on top.'
+  },
+  {
+    type: 'Flat White',
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
+    description: 'A type of coffee made with espresso and hot steamed milk, but without the froth characteristic of a cappuccino.'
+  },
+  {
+    type: 'Cold Press',
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
+    description: 'Cold brew, or cold press, is coffee grounds steeped in water at cold or room temperature for an extended period.'
+  },
+  {
+    type: 'Macchiato',
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
+    description: 'Macchiato, meaning "stained", is an espresso with a dash of foamed milk. At first sight it resembles a small cappuccino, but even if the ingredients are the same as those used for cappuccino, a macchiato has a much stronger and aromatic taste.'
+  },
+  {
+    type: 'Chai Latte',
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
+    description: 'Numerous houses use the term chai latte to indicate that the steamed milk of a normal caffè latte is being flavoured with a spiced tea concentrate instead of with espresso.'
+  },
+  {
+    type: 'Mochaccino',
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/99/94/fe/9994fedb8db160d363719f2acb74acb4.jpg',
+    description: 'Single shot poured over heaped teaspoon of hot chocolate powder and stirred. Top with velvety milk in a cappuccino style.'
+  },
+]
