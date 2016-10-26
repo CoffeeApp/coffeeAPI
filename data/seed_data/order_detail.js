979f6b96ac079433a0235ca0968d178958abb8e7
Thomas & Lisa <pair+TJRobson.lisa-seeto@devbootcamp.com>
2016-10-26 15:23:40 +1300

    changes

--- data/seed_data/order_detail.js
+++ data/seed_data/order_detail.js
@@ -8,7 +8,7 @@ module.exports = [
     note: ''
   },
   {
-    order_id: 1,
+    order_id: 2,
     coffee_id: 2,
     quantity:2,
     milk:'Trim milk',
@@ -16,7 +16,7 @@ module.exports = [
     note: 'Extra hot'
   },
   {
-    order_id: 1,
+    order_id: 3,
     coffee_id: 3,
     quantity:2,
     milk:'Whole milk',
@@ -24,7 +24,7 @@ module.exports = [
     note: 'Milk on the side'
   },
   {
-    order_id: 2,
+    order_id: 4,
     coffee_id: 7,
     quantity:2,
     milk:'No milk',
@@ -32,7 +32,7 @@ module.exports = [
     note: ''
   },
   {
-    order_id: 2,
+    order_id: 5,
     coffee_id: 5,
     quantity:2,
     milk:'Whole milk',
@@ -40,7 +40,7 @@ module.exports = [
     note: 'Not too strong'
   },
   {
-    order_id: 2,
+    order_id: 6,
     coffee_id: 3,
     quantity:2,
     milk:'No milk',
@@ -48,7 +48,7 @@ module.exports = [
     note: ''
   },
   {
-    order_id: 3,
+    order_id: 7,
     coffee_id: 4,
     quantity:2,
     milk:'Trim milk',
@@ -56,7 +56,7 @@ module.exports = [
     note: ''
   },
   {
-    order_id: 3,
+    order_id: 8,
     coffee_id: 10,
     quantity:1,
     milk:'Trim milk',
@@ -64,7 +64,7 @@ module.exports = [
     note: 'Extra shot'
   },
   {
-    order_id: 3,
+    order_id: 9,
     coffee_id: 6,
     quantity:1,
     milk:'Soy milk',
@@ -72,7 +72,103 @@ module.exports = [
     note: 'No chocolate dusting'
   },
   {
-    order_id: 4,
+    order_id: 10,
+    coffee_id: 8,
+    quantity:1,
+    milk:'Soy milk',
+    sugar:3,
+    note: ''
+  },
+  {
+    order_id: 11,
+    coffee_id: 2,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:3,
+    note: ''
+  },
+  {
+    order_id: 12,
+    coffee_id: 6,
+    quantity:2,
+    milk:'Almond milk',
+    sugar:3,
+    note: 'Extra hot'
+  },
+  {
+    order_id: 13,
+    coffee_id: 1,
+    quantity:1,
+    milk:'No milk',
+    sugar:1,
+    note: ''
+  },
+  {
+    order_id: 14,
+    coffee_id: 2,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:2,
+    note: 'Extra hot'
+  },
+  {
+    order_id: 15,
+    coffee_id: 3,
+    quantity:2,
+    milk:'Whole milk',
+    sugar:1,
+    note: 'Milk on the side'
+  },
+  {
+    order_id: 16,
+    coffee_id: 7,
+    quantity:2,
+    milk:'No milk',
+    sugar:0,
+    note: ''
+  },
+  {
+    order_id: 17,
+    coffee_id: 5,
+    quantity:2,
+    milk:'Whole milk',
+    sugar:0,
+    note: 'Not too strong'
+  },
+  {
+    order_id: 18,
+    coffee_id: 3,
+    quantity:2,
+    milk:'No milk',
+    sugar:2,
+    note: ''
+  },
+  {
+    order_id: 19,
+    coffee_id: 4,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:0,
+    note: ''
+  },
+  {
+    order_id: 20,
+    coffee_id: 10,
+    quantity:1,
+    milk:'Trim milk',
+    sugar:2,
+    note: 'Extra shot'
+  },
+  {
+    order_id: 21,
+    coffee_id: 6,
+    quantity:1,
+    milk:'Soy milk',
+    sugar:1,
+    note: 'No chocolate dusting'
+  },
+  {
+    order_id: 2,
     coffee_id: 8,
     quantity:1,
     milk:'Soy milk',
@@ -88,11 +184,227 @@ module.exports = [
     note: ''
   },
   {
-    order_id: 4,
+    order_id: 6,
     coffee_id: 6,
     quantity:2,
     milk:'Almond milk',
     sugar:3,
     note: 'Extra hot'
+  },
+  {
+    order_id: 8,
+    coffee_id: 1,
+    quantity:1,
+    milk:'No milk',
+    sugar:1,
+    note: ''
+  },
+  {
+    order_id: 10,
+    coffee_id: 2,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:2,
+    note: 'Extra hot'
+  },
+  {
+    order_id: 12,
+    coffee_id: 3,
+    quantity:2,
+    milk:'Whole milk',
+    sugar:1,
+    note: 'Milk on the side'
+  },
+  {
+    order_id: 14,
+    coffee_id: 7,
+    quantity:2,
+    milk:'No milk',
+    sugar:0,
+    note: ''
+  },
+  {
+    order_id: 16,
+    coffee_id: 5,
+    quantity:2,
+    milk:'Whole milk',
+    sugar:0,
+    note: 'Not too strong'
+  },
+  {
+    order_id: 18,
+    coffee_id: 3,
+    quantity:2,
+    milk:'No milk',
+    sugar:2,
+    note: ''
+  },
+  {
+    order_id: 20,
+    coffee_id: 4,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:0,
+    note: ''
+  },
+  {
+    order_id: 21,
+    coffee_id: 10,
+    quantity:1,
+    milk:'Trim milk',
+    sugar:2,
+    note: 'Extra shot'
+  },
+  {
+    order_id: 5,
+    coffee_id: 6,
+    quantity:1,
+    milk:'Soy milk',
+    sugar:1,
+    note: 'No chocolate dusting'
+  },
+  {
+    order_id: 10,
+    coffee_id: 8,
+    quantity:1,
+    milk:'Soy milk',
+    sugar:3,
+    note: ''
+  },
+  {
+    order_id: 15,
+    coffee_id: 2,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:3,
+    note: ''
+  },
+  {
+    order_id: 20,
+    coffee_id: 6,
+    quantity:2,
+    milk:'Almond milk',
+    sugar:3,
+    note: 'Extra hot'
+  },
+  {
+    order_id: 8,
+    coffee_id: 1,
+    quantity:1,
+    milk:'No milk',
+    sugar:1,
+    note: ''
+  },
+  {
+    order_id: 1,
+    coffee_id: 2,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:2,
+    note: 'Extra hot'
+  },
+  {
+    order_id: 17,
+    coffee_id: 3,
+    quantity:2,
+    milk:'Whole milk',
+    sugar:1,
+    note: 'Milk on the side'
+  },
+  {
+    order_id: 8,
+    coffee_id: 7,
+    quantity:2,
+    milk:'No milk',
+    sugar:0,
+    note: ''
+  },
+  {
+    order_id: 13,
+    coffee_id: 5,
+    quantity:2,
+    milk:'Whole milk',
+    sugar:0,
+    note: 'Not too strong'
+  },
+  {
+    order_id: 17,
+    coffee_id: 3,
+    quantity:2,
+    milk:'No milk',
+    sugar:2,
+    note: ''
+  },
+  {
+    order_id: 8,
+    coffee_id: 4,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:0,
+    note: ''
+  },
+  {
+    order_id: 3,
+    coffee_id: 10,
+    quantity:1,
+    milk:'Trim milk',
+    sugar:2,
+    note: 'Extra shot'
+  },
+  {
+    order_id: 2,
+    coffee_id: 6,
+    quantity:1,
+    milk:'Soy milk',
+    sugar:1,
+    note: 'No chocolate dusting'
+  },
+  {
+    order_id: 15,
+    coffee_id: 8,
+    quantity:1,
+    milk:'Soy milk',
+    sugar:3,
+    note: ''
+  },
+  {
+    order_id: 21,
+    coffee_id: 2,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:3,
+    note: ''
+  },
+  {
+    order_id: 16,
+    coffee_id: 6,
+    quantity:2,
+    milk:'Almond milk',
+    sugar:3,
+    note: 'Extra hot'
+  },
+  {
+    order_id: 13,
+    coffee_id: 1,
+    quantity:1,
+    milk:'No milk',
+    sugar:1,
+    note: ''
+  },
+  {
+    order_id: 7,
+    coffee_id: 2,
+    quantity:2,
+    milk:'Trim milk',
+    sugar:2,
+    note: 'Extra hot'
+  },
+  {
+    order_id: 11,
+    coffee_id: 3,
+    quantity:2,
+    milk:'Whole milk',
+    sugar:1,
+    note: 'Milk on the side'
   }
 ]
